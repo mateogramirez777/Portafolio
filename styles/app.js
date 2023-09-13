4d754724bdd5fd1ad5f497a1c0683bcb38ca4738
Mateo Giraldo <141374437+mateogramirez777@users.noreply.github.com>
2023-09-12 20:05:17 -0500

    Add files via upload

---
+++ styles/app.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function(){
+    document.getElementById("btn1").addEventListener("click", function(){
+        alert("Esta siendo dirigido a mi chat de whatsapp");
+    }) 
+})
